a4fb3f6784b8eb8d5fbbe8d0a2b182ff4d54cdd6
Godslove Obiagwu <s3812709@students.fscj.edu>
2026-09-14 17:16:31 -0400

    Add files via upload

---
+++ M2Project/src/test/java/edu/fscj/cen3024c/taskmanager/M2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fscj.cen3024c.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
